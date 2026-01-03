5ed393ead5f81fc1c3622152ee405fe7af6c3821
Deemas97 <91252222+Deemas97@users.noreply.github.com>
2026-01-04 00:08:42 +0400

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,6 @@ define('YADRO_PHP__ROOT_DIR', realpath(__DIR__ . '/../'));
 
 $autoloader = require (YADRO_PHP__ROOT_DIR . '/src/Bootstrap/Autoloader.php');
 
-use ZeroPHP\Kernel;
+use YadroPHP\Kernel;
 
 return $response = (new Kernel())->handle();
